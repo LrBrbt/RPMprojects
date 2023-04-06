99d0ced235b5ad86d53401eaf458ecf63796963b
Valeria <114875225+LrBrbt@users.noreply.github.com>
2023-04-06 19:26:19 +0300

    Add files via upload

---
+++ project9/9практическая/Computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9практическая
+{
+    struct Computer
+    {
+        public string ComputerName;
+        public string ProcessorType;
+        public int MemorySize;
+        public int HDDSize;
+        public string VideoCard;
+
+        public Computer(string comtuterName, string processorType, int memorySize, int hardDiskDriveSize, string videoCard)
+        {
+            ComputerName = comtuterName;
+            ProcessorType = processorType;
+            MemorySize = memorySize;
+            HDDSize = hardDiskDriveSize;
+            VideoCard = videoCard;
+        }
+        public override string ToString()
+        {
+            return $"{ComputerName}";
+        }
+    }
+}
---
+++ project9/9практическая/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _9практическая
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Computer computer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            computer = new("Компьютер", textboxProcessorType.Text, Int32.Parse(textboxMemorySize.Text), Int32.Parse(textboxHDDSize.Text), textboxVideoCard.Text);
+            Computers.Items.Add(computer);
+        }
+
+        private void Computers_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedComputer = (Computer)Computers.SelectedItem;
+            MessageBox.Show("Тип процессора: " + selectedComputer.ProcessorType + "\n" + "Объем памяти: " +  selectedComputer.MemorySize.ToString() + "\n" + "Объем жесткого диска: " + selectedComputer.HDDSize.ToString() + "\n" + "Видеокарта: " + selectedComputer.VideoCard);
+        }
+
+        private void Average_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedComputers = Computers.SelectedItems;
+            int sum = 0;
+            for (int i = 0; i < selectedComputers.Count; i++)
+            {
+                Computer computer = (Computer)selectedComputers[i];
+                sum += computer.MemorySize;
+            }
+            int average = sum / selectedComputers.Count;
+            MessageBox.Show("Среднее значение памяти: " + average.ToString());
+        }
+    }
+}
