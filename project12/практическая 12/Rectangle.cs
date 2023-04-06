6bdb8e950f76a5a4e82d0a4b64fcb360b3d367b5
Valeria <114875225+LrBrbt@users.noreply.github.com>
2023-04-06 19:45:03 +0300

    Add files via upload

---
+++ project12/практическая 12/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace практическая_12
+{
+    class Rectangle
+    {
+        private int _length;
+        private int _width;
+
+        public int Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public Rectangle(int length, int width)
+        {
+                _length = length;
+                _width = width;
+        }
+
+        public int Perimeter(int length, int width)
+        {
+            return 2 * (length + width);
+        }
+        public int Area(int length, int width)
+        {
+            return length * width;
+        }
+    }
+}
